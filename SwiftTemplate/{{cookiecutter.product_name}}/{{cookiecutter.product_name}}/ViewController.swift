51a06e3c73fca9c27d8509f3ff70a92d95b99cfb
ethanz <ethanz1990@163.com>
2019-05-14 11:01:35 +0800

    import project

---
+++ SwiftTemplate/{{cookiecutter.product_name}}/{{cookiecutter.product_name}}/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{cookiecutter.product_name}}
+//
+//  Created by Ethanz on 2019/5/14.
+//  Copyright © 2019 {{cookiecutter.organization_name}}. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
